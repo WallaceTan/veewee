46c13990a428fda3eed59eb027a97f6581523e72
Wallace Tan <wallace_tan@ida.gov.sg>
2015-09-16 18:47:55 +0800

    Replace 'veewee' with 'vagrant'. Add 'net-tools' package.

--- definitions/CentOS-7-x86_64/base.sh
+++ definitions/CentOS-7-x86_64/base.sh
@@ -2,15 +2,17 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-cat > /etc/yum.repos.d/epel.repo << EOM
-[epel]
-name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/beta/7/\$basearch
-enabled=1
-gpgcheck=0
-EOM
+# cat > /etc/yum.repos.d/epel.repo << EOM
+# [epel]
+# name=Extra Packages for Enterprise Linux 7 - $basearch
+# baseurl=http://download.fedoraproject.org/pub/epel/7/\$basearch
+# mirrorlist=https://mirrors.fedoraproject.org/metalink\?repo=epel-7\&arch=\$basearch
+# enabled=1
+# gpgcheck=0
+# EOM
 
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
+yum -y install epel-release
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils net-tools
 
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
